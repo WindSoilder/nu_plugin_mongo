c540a5a37360b94e8b1c1a7fb488e452adb8dd28
WindSoilder <WindSoilder@outlook.com>
2024-10-23 13:50:54 +0800

    update doc

--- README.md
+++ README.md
@@ -17,6 +17,17 @@ I want to have more lite weight tool to explore data.  So here is the plugin.
 - All I want to do is query and explore data, so the plugin includes query commands only.
 - You might note that the command prefix is `mongoc`, not `mongo`.  Because I don't want to make the name conflict with official lagacy [mongo](https://www.mongodb.com/docs/manual/reference/mongo/) cli.
 
+## Usage
+
+1. First, you need to have latest nushell installed(Of cause it's a nushell plugin).
+2. Then you can install from `cargo`
+```
+cargo install nu_plugin_mongo
+```
+3. Add and use the plugin: `plugin add <nu_plugin_mongo_path>; plugin use mongo`
+4. Then you will gain some `mongoc` commands to play with.
+
+
 ## Full help
 
 ```nushell
@@ -33,7 +44,8 @@ Subcommands:
   mongoc find - find mongodb documents
   mongoc find-one - find mongodb documents
   mongoc list - list mongodb connections
-  mongoc list-collection-names - list all available collection names
+  mongoc list-colls - list all available collection names
+  mongoc list-indexes - find mongodb documents
   mongoc open - open mongodb connection, the url must contains default databse
   mongoc remove - remove mongodb handles
   mongoc select - select current mongodb handle
@@ -46,7 +58,5 @@ Input/output types:
   │ # │  input  │ output │
   ├───┼─────────┼────────┤
   │ 0 │ nothing │ string │
-  ├───┼─────────┼────────┤
-  │ # │  input  │ output │
   ╰───┴─────────┴────────╯
 ```
---
+++ src/cmds/list_indexes.rs
@@ -0,0 +1,85 @@
+use crate::MongoPlugin;
+use bson::ser::to_document;
+use mongodb::bson::Document;
+use nu_plugin::SimplePluginCommand;
+use nu_protocol::{
+    record, Category, Example, LabeledError, Signature, Spanned, SyntaxShape, Type, Value,
+};
+
+pub struct ListIndexes;
+
+impl SimplePluginCommand for ListIndexes {
+    type Plugin = MongoPlugin;
+
+    fn name(&self) -> &str {
+        "mongoc list-indexes"
+    }
+
+    fn description(&self) -> &str {
+        "find mongodb documents"
+    }
+
+    fn signature(&self) -> nu_protocol::Signature {
+        Signature::build("mongoc list-indexes")
+            .required_named(
+                "collection",
+                SyntaxShape::String,
+                "collection name",
+                Some('c'),
+            )
+            .named(
+                "db-handle",
+                SyntaxShape::Int,
+                "database handle, can get from `mongoc list`",
+                Some('d'),
+            )
+            .input_output_type(Type::Nothing, Type::table())
+            .category(Category::Database)
+    }
+
+    fn examples(&self) -> Vec<Example> {
+        vec![Example {
+            description: "list indexes for collection `students`",
+            example: "mongoc list-indexes students",
+            result: None,
+        }]
+    }
+    fn run(
+        &self,
+        plugin: &MongoPlugin,
+        _engine: &nu_plugin::EngineInterface,
+        call: &nu_plugin::EvaluatedCall,
+        _input: &Value,
+    ) -> Result<Value, LabeledError> {
+        let db_handle: Option<Spanned<i64>> = call.get_flag("db-handle")?;
+        let db = match db_handle {
+            None => plugin.get_handle(plugin.get_current()?, call.head)?,
+            Some(db_handle) => plugin.get_handle(db_handle.item as u8, db_handle.span)?,
+        };
+        let coll: String = call
+            .get_flag("collection")?
+            .expect("already check existed.");
+        let result = db
+            .collection::<Document>(&coll)
+            .list_indexes()
+            .run()
+            .map_err(|e| LabeledError::new(format!("{e}")))?;
+
+        let mut rows = vec![];
+        for doc in result {
+            let doc = doc.map_err(|e| LabeledError::new(format!("{e}")))?;
+            let rec = Value::record(
+                record! {
+                    "key" => Value::string(doc.keys.to_string(), call.head),
+                    "options" => match doc.options {
+                        None => Value::nothing(call.head),
+                        Some(opt) => Value::string(to_document(&opt).unwrap().to_string(), call.head),
+                    }
+                },
+                call.head,
+            );
+            rows.push(rec)
+        }
+        Ok(Value::list(rows, call.head))
+    }
+}
